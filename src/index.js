f2ed8468d0db6de7d76bc0eac94e73d235201017
TrungNNg <tnguy384@mail.ccsf.edu>
2020-08-19 22:06:16 -0400

    connect Firebase, Signin and Signup form

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+    apiKey: "AIzaSyAS6psntytii76FmMbJxcDvRISaQiLNLHA",
+    authDomain: "netflix-clone-e128e.firebaseapp.com",
+    databaseURL: "https://netflix-clone-e128e.firebaseio.com",
+    projectId: "netflix-clone-e128e",
+    storageBucket: "netflix-clone-e128e.appspot.com",
+    messagingSenderId: "782717606691",
+    appId: "1:782717606691:web:daf5050b91f72116004a96"
+}
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>, 
     document.getElementById('root')
 );
